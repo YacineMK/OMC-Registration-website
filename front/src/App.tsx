cea624d790edacf857eafe693d9fe0471b5d5b9e
YacineMK <merroukiyacine06@gmail.com>
2023-11-10 09:40:19 +0100

    add form

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,15 @@
 import './App.css';
 
+function Inputs() {
+  return (
+    <div className="input-container">
+      <label >1 -> Hi, What’s your full name ?:</label>
+      <input type="text" placeholder="Type your answer here..."/>
+      <button><span>Next</span><img src="/public/fli.svg" alt=""/></button>
+    </div>
+  );
+}
+
 function App() {
   return (
     <>
@@ -7,6 +17,14 @@ function App() {
         <img src='/omclogo.png' alt="OMC Logo" />
         <h1>Register Now</h1>
         <p>Fuel Your Mind and Join the Journey!</p>
+      </div>
+      <div className="inputs-section">
+        <Inputs />
+        <Inputs />
+        <Inputs />
+        <Inputs />
+        <Inputs />
+
       </div>
     </>
   );
