2b0bc6c49b4dc77bc6a329a5440c7579613f1fd6
YacineMK <merroukiyacine06@gmail.com>
2023-11-10 14:22:21 +0100

    finish form

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,41 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function Inputs() {
+function Input({ qst }: { qst: string }) {
   return (
     <div className="input-container">
-      <label >1 -> Hi, What’s your full name ?:</label>
-      <input type="text" placeholder="Type your answer here..."/>
-      <button><span>Next</span><img src="/public/fli.svg" alt=""/></button>
+      <label>{qst}</label>
+      <input type="text" placeholder="Type your answer here..." />
+      <button>
+        <span>Next</span>
+        <img src="/public/fli.svg" alt="" />
+      </button>
+    </div>
+  );
+}
+
+function Select({ qst }: { qst: string }) {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div className="input-container">
+      <label>{qst}</label>
+      <select value={selectedOption} onChange={handleOptionChange}>
+        <option value="">Select Your Study year</option>
+        <option value="option1">L1</option>
+        <option value="option2">L2</option>
+        <option value="option3">L3</option>
+        <option value="option4">M1</option>
+        <option value="option5">M2</option>
+      </select>
+      <button>
+        <span>Next</span>
+        <img src="/public/fli.svg" alt="" />
+      </button>
     </div>
   );
 }
@@ -14,22 +44,26 @@ function App() {
   return (
     <>
       <div className="text-div">
-        <img src='/omclogo.png' alt="OMC Logo" />
+        <img src="/omclogo.png" alt="OMC Logo" />
         <h1>Register Now</h1>
         <p>Fuel Your Mind and Join the Journey!</p>
       </div>
       <div className="inputs-section">
-        <Inputs qst="1 -> Hi, What’s your full name ?:"/>
-        <Inputs qst="2 -> What’s your Email ?"/>
-        <Inputs qst="3 -> What's your Phone Number ?"/>
-        <Inputs qst="4 -> What's your Discord id?"/>
-        <Inputs qst="5 -> What's you know about Open Source ?"/>
-        <Inputs qst="6 -> What's your skills"/>
-        <Inputs qst="7 -> How you know OMC ?"/>
-        <Inputs qst="8 -> What do you know about OMC ?"/>
-        <Inputs qst="9 -> What do you know about OMC ?"/>
-
-
+        <Input qst="1 -> Hi, What’s your full name ?:" />
+        <Input qst="2 -> What’s your Email ?" />
+        <Input qst="3 -> What's your Phone Number ?" />
+        <Input qst="4 -> What is your University" />
+        <Input qst="5 -> What is your Study field" />
+        <Select qst="6 -> Year of Study ?" />
+        <Input qst="7 -> What's your Phone Number ?" />
+        <Input qst="8 -> What's your Discord id?" />
+        <Input qst="9 -> What you know about Open Source ?" />
+        <Input qst="10 -> What's your skills ?" />
+        <Input qst="11 -> Your GitHub / LinkedIn or Portfolio?" />
+        <Input qst="12 -> How you know OMC ?" />
+        <Input qst="13 -> What do you know about OMC ?" />
+        <Input qst="14 -> Why You want to join OMC ?" />
+        <Input qst="15 -> Anything to Add ?" />
       </div>
     </>
   );
